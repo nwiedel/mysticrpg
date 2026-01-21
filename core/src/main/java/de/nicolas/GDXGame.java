8817620ea1f700e784c0745126ad0f39ed29783b
nwiedel <164350832+nwiedel@users.noreply.github.com>
2026-01-21 23:44:24 +0100

    Video 3 Screens
    Anfang

--- core/src/main/java/de/nicolas/GDXGame.java
+++ core/src/main/java/de/nicolas/GDXGame.java
@@ -1,11 +1,25 @@
 package de.nicolas;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class GDXGame extends Game {
+
+    private final Map<Class<? extends Screen>, Screen> screenCache = new HashMap<>();
+
     @Override
     public void create() {
         setScreen(new FirstScreen());
+    }
+
+    public void addScreen(Screen screen){
+        if (screenCache.containsValue(screen)){
+            return;
+        }
+        screenCache.put(screen.getClass(), screen);
     }
 }
